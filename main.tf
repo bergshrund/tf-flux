cc975892f52141b8e59646024cd21a4d7f6fddad
Andrew Ivanov <bergshrund@#gmail.com>
2023-12-21 11:43:17 +0200

    Added github-repository and tls modules

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 module "kind_cluster" {
-  source = "./modules/kind-cluster"
+  source = "./modules/tf-kind-cluster"
+}
+
+module "github_repository" {
+  source                   = "./modules/tf-github-repository"
+  github_account           = var.github_account
+  github_access_token      = var.github_access_token
+  repository_name          = var.github_repo
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+
+module "tls_private_key" {
+  source    = "./modules/tf-hashicorp-tls-key"
+  algorithm = "RSA"
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "github_account" {
+  type        = string
+  description = "The GitHub account that own repository"
+}
+
+variable "github_access_token" {
+  type        = string
+  description = "GitHub access token"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "Flux configuration repo"
+}
