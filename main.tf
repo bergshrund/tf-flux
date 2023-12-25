ece451be7c456810b580765fa54cfab4f194c9fa
Andrii Ivanov <bergshrund@gmail.com>
2023-12-25 22:22:31 +0000

    Added new output variable the the tf-gke module

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ provider "github" {
   token = var.github_access_token
 }
 
-
 module "gke_cluster" {
    source = "./modules/tf-gke"
    project_id = "data1co"
@@ -17,36 +16,34 @@ module "gke_cluster" {
    pods_subnetwork_range = "10.10.0.0/16"
    svc_subnetwork_range = "10.20.0.0/20"
    node_pool_name = "default-pool"
-   node_poll_machine_type = "e2-micro"
-   node_poll_size = "1"
+   node_poll_machine_type = "n2-standard-2"
+   node_poll_size = "2"
 }
-
-
 
 #module "kind_cluster" {
 #  source = "./modules/tf-kind-cluster"
 #}
 
-#module "flux_bootstrap" {
-#  source              = "./modules/tf-flux-bootstrap"
-#  kubeconfig_paths    = module.kind_cluster.kubeconfig_path
-#  kubeconfig_context  = module.kind_cluster.context
-#  github_account      = var.github_account
-#  github_repository   = var.github_repo
-#  github_access_token = var.github_access_token
-#  private_key         = module.tls_private_key.private_key_pem
-#  public_key          = module.tls_private_key.public_key_openssh
-#}
+module "flux_bootstrap" {
+  source              = "./modules/tf-flux-bootstrap"
+  kubeconfig_paths    = module.gke_cluster.kubeconfig_path
+  kubeconfig_context  = module.gke_cluster.context
+  github_account      = var.github_account
+  github_repository   = var.github_repo
+  github_access_token = var.github_access_token
+  private_key         = module.tls_private_key.private_key_pem
+  public_key          = module.tls_private_key.public_key_openssh
+}
 
-#module "kbot" {
-#  depends_on = [ module.flux_bootstrap.flux_id ]
-#  source              = "./modules/tf-kbot"
-#  repository_name     = var.github_repo
-#  target_path         = module.flux_bootstrap.flux_path
-#  files = ["kbot/kbot-ns.yaml","kbot/kbot-repo.yaml","kbot/kbot-helmrelease.yaml"]
-#}
+module "kbot" {
+  depends_on = [ module.flux_bootstrap.flux_id ]
+  source              = "./modules/tf-kbot"
+  repository_name     = var.github_repo
+  target_path         = module.flux_bootstrap.flux_path
+  files = ["kbot/kbot-ns.yaml","kbot/kbot-repo.yaml","kbot/kbot-helmrelease.yaml"]
+}
 
-#module "tls_private_key" {
-#  source    = "./modules/tf-hashicorp-tls-key"
-#  algorithm = "RSA"
-#}
+module "tls_private_key" {
+  source    = "./modules/tf-hashicorp-tls-key"
+  algorithm = "RSA"
+}
--- modules/tf-gke/outputs.tf
+++ modules/tf-gke/outputs.tf
@@ -3,6 +3,11 @@ output "kubeconfig_path" {
   description = "The path to the kubeconfig file"
 }
 
+output "context" {
+  value       = module.gke.name
+  description = "The name of the context in kubeconfig"
+}
+
 
 
 
