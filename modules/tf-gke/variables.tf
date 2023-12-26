6e504ee904524b604f4cdac7ceaf4f015b1ad8a2
Andrew Ivanov <bergshrund@#gmail.com>
2023-12-26 21:49:59 +0200

    Change flux tracked repo to develop. Add vars descriptions

--- modules/tf-gke/variables.tf
+++ modules/tf-gke/variables.tf
@@ -1,69 +1,70 @@
 variable "project_id" {
-  description = ""
+  description = "The project ID to host the cluster in"
   type        = string
 }
 
 variable "impersonate_service_account" {
-  description = ""
+  description = "The service account to impersonate for all Google API Calls"
   type        = string
 }
 
 variable "region" {
-  description = ""
+  description = "The region to host the cluster in"
   type        = string
 }
 
 variable "zone" {
-  description = ""
+  description = "The zones to host the cluster in"
   type        = string
 }
 
 variable "cluster_name" {
-  description = ""
+  description = "The name of the cluster"
   type        = string
 }
 
 variable "vpc_network_name" {
-  description = ""
+  description = "The VPC network to host the cluster in"
   type        = string
 }
 
 variable "vpc_subnetwork_name" {
-  description = ""
+  description = "The subnetwork to host the cluster in"
   type        = string
 }
 
 variable "vpc_subnetwork_range" {
-  description = ""
+  description = "The IP address range of the kubernetes nodes in this cluster"
   type        = string
 }
 
 variable "pods_subnetwork_range" {
-  description = ""
+  description = "The IP address range of the kubernetes pods in this cluster"
   type        = string
 }
 
 variable "svc_subnetwork_range" {
-  description = ""
+  description = "The IP address range of the kubernetes services in this cluster"
   type        = string
 }
 
 variable "node_pool_name" {
-  description = ""
+  description = "The name of the node pool"
   type        = string
 }
 
 variable "node_poll_machine_type" {
-  description = ""
+  description = "The name of a Google Compute Engine machine type"
   type        = string
 }
 
 variable "node_poll_size" {
-  description = ""
+  description = "The number of nodes in the nodepool"
   type        = number
 }
 
 variable "cluster_service_account_iam_roles" {
+  description = "List of IAM roles to assign to the default Kubernetes cluster service account."
   type = list(string)
   default = [
     "roles/logging.logWriter",
@@ -71,12 +72,11 @@ variable "cluster_service_account_iam_roles" {
     "roles/monitoring.viewer",
     "roles/stackdriver.resourceMetadata.writer"
   ]
-  description = "List of IAM roles to assign to the default Kubernetes cluster service account."
 }
 
 variable "cluster_service_account_custom_iam_roles" {
+  description = "List of arbitrary additional IAM roles to attach to the service account on the Kubernetes clusters nodes."
   type        = list(string)
   default     = []
-  description = "List of arbitrary additional IAM roles to attach to the service account on the Kubernetes clusters nodes."
 }
 
--- variables.tf
+++ variables.tf
@@ -15,70 +15,70 @@ variable "github_repo" {
 
 variable "project_id" {
   type        = string
-  description = ""
+  description = "The project ID to host the cluster in"
 }
 
 variable "region" {
   type        = string
-  description = ""
+  description = "The region to host the cluster in"
 }
 
 variable "zone" {
   type        = string
-  description = ""
+  description = "The zones to host the cluster in"
 }
 
 variable "cluster_name" {
   type        = string
-  description = ""
+  description = "The name of the cluster"
 }
 
 variable "vpc_network_name" {
   type        = string
-  description = ""
+  description = "The VPC network to host the cluster in"
 }
 
 variable "vpc_subnetwork_name" {
   type        = string
-  description = ""
+  description = "The subnetwork to host the cluster in"
 }
 
 variable "pods_subnetwork_range" {
   type        = string
-  description = ""
+  description = "The IP address range of the kubernetes pods in this cluster"
 }
 
 variable "svc_subnetwork_range" {
   type        = string
-  description = ""
+  description = "The IP address range of the kubernetes services in this cluster"
 }
 
 variable "node_pool_name" {
   type        = string
-  description = ""
+  description = "The name of the node pool"
 }
 
 variable "node_poll_machine_type" {
   type        = string
-  description = ""
+  description = "The name of a Google Compute Engine machine type"
 }
 
 variable "node_poll_size" {
   type        = number
-  description = ""
+  description = "The number of nodes in the nodepool"
 }
 
 variable "impersonate_service_account" {
   type        = string
-  description = ""
+  description = "The service account to impersonate for all Google API Calls"
 }
 
 variable "vpc_subnetwork_range" {
   type        = string
-  description = ""
+  description = "The IP address range of the kubernetes nodes in this cluster"
 }
 
 variable "kbot_token" {
   type        = string
-  description = ""
+  description = "Kbot token"
 }
