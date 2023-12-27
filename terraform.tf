93f92f08f7572319c382e6e328c0bca0972d5cef
Andrii Ivanov <bergshrund@gmail.com>
2023-12-27 15:59:22 +0000

    Added state storage backet and reconcile strategy has been set to Revision

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "5.42.0"
     }
   }
+
+  backend "gcs" {
+    bucket  = "data1-terraform-state"
+    prefix  = "develop/kbot"
+    impersonate_service_account = "devops@data1co.iam.gserviceaccount.com"
+  }
 }
